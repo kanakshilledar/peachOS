52efb3bf1514b1c3e1f3a3d050483ed04921d5ee
Kanak Shilledar <ideatic_memory.cv0l1@aleeas.com>
2022-12-31 21:49:48 +0530

    basic booloader created

--- boot.asm
+++ boot.asm
@@ -1,10 +1,25 @@
-ORG 0x7c00
+ORG 0x00
 BITS 16
+_start:
+    jmp short start
+    nop
 
+ times 33 db 0
 start:
+    jmp 0x7C0:step2
+
+step2:
+    cli             ; clear interrupts
+    mov ax, 0x7C0
+    mov ds, ax
+    mov es, ax
+    mov ax, 0x00
+    mov ss, ax
+    mov sp, 0x7C00
+    sti             ; enables interrupts
+
     mov si, message
     call print
-
     jmp $
 
 print:
