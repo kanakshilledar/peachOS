3b4f1daae7f269b844899aa67c8793553ee3c53d
Kanak Shilledar <ideatic_memory.cv0l1@aleeas.com>
2023-01-11 14:34:00 +0530

    printing characters on screen. getting bug with `terminal_make_char()` function. unable to print characters without `terminal_make_char()` able to print the characters.

--- src/kernel.c
+++ src/kernel.c
@@ -1,5 +1,9 @@
 #include "kernel.h"
-
+#include <stdint.h>
+uint16_t terminal_make_char(char c, char color) {
+    return (color << 8) | c;
+}
 void kernel_main() {
-
+    uint16_t * video_mem = (uint16_t *)(0xB8000);         // pointer for colored ascii characters
+    video_mem[0] = terminal_make_char('C', 3);
 }
