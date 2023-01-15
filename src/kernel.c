17ae355fefcab0ad730a9f91f2a7fafaefe11a10
Kanak Shilledar <ideatic_memory.cv0l1@aleeas.com>
2023-01-15 13:38:21 +0530

    implemented page table modification added an sample example in `kernel.c` to check for paging

--- src/kernel.c
+++ src/kernel.c
@@ -68,6 +68,17 @@ void kernel_main() {
     kernel_chunk = paging_new_4gb(PAGING_IS_WRITABLE | PAGING_IS_PRESENT | PAGING_ACCESS_FROM_ALL);
     // switch to kernel_paging_chunk
     paging_switch(paging_4gb_chunk_get_directory(kernel_chunk));
+
+    char* ptr = kzalloc(4096);
+    paging_set(paging_4gb_chunk_get_directory(kernel_chunk), (void*)0x1000, (uint32_t)ptr | PAGING_ACCESS_FROM_ALL | PAGING_IS_PRESENT | PAGING_IS_WRITABLE);
+
     enable_paging();                    // enable paging
+
+    char* ptr2 = (char*) 0x1000;
+    ptr2[0] = 'A';
+    ptr2[1] = 'B';
+    print(ptr);
+    print(ptr2);
+
     enable_interrupts();                // enable interrupts
 }
